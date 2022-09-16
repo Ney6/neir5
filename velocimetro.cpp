12a31de4a5d4f0f3976f664eeb67146e010c771d
pelotero10 <neil.canchingre.tamayo@utelvt.edu.ec>
2022-09-16 09:14:20 -0500

    velocimetro1

---
+++ velocimetro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        float v, d, t;
+        cout<<"ingrese la distancia d= ";cin>>d;
+        cout<<"ingrese el tiempo t= "; cin>>t;
+        v=d/t;
+        cout<<"la velocidad calculafa es de v= "<<v<<endl<<endl;
+        return 0;
+         }
