bc7e1823946b391f6d3fde2c9374ceb9b3bc60ba
pelotero10 <neil.canchingre.tamayo@utelvt.edu.ec>
2022-09-24 18:27:35 -0500

    calculo1

---
+++ compra.cpp
@@ -0,0 +1,23 @@
+# incluir < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal ()
+{
+	int i= 0 ,l;
+	flotante x,s= 0 ;
+	cout<< " ingrese el limite l= " ; cin>>l;
+	hacer {
+
+	cout<< " ingrese el numero x= " ; cin>>x;
+	yo = yo + 1 ;
+	s=s+x;
+
+
+
+	} mientras (i<l);
+	cout<< " Se ingresaron " <<l<< " numeros " << " que sumaron " <<s<<endl;
+	devolve 0;
+
+
+
+}
